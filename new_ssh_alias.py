86d387e9b6c93c917cc3d545662f5b965d004f0a
zeke-chin <zeke-chin@icloud.com>
2023-11-07 20:07:08 +0800

    add: 添加走代理功能

--- new_ssh_alias.py
+++ new_ssh_alias.py
@@ -1,8 +1,9 @@
+import time
 from pathlib import Path
 
 
 class SSHBuilder:
-    def __init__(self, host, user, password, alias_name, port=22, file_home=""):
+    def __init__(self, host, user, password, alias_name, port, file_home, use_proxy, proxy):
         self.host = host
         self.user = user
         self.password = password
@@ -10,12 +11,14 @@ class SSHBuilder:
         self.port = port
         self.file_home = Path.home() / ".ssh/home" if file_home == "" else Path(file_home)
         self.ssh_file_path = self.file_home / self.alias_name
+        self.use_proxy = use_proxy
+        self.proxy = proxy
 
     # def build(self):
     #     self.write_ssh_file()
     #
     #     print("正在修改shellrc文件")
-        # self.write_shellrc()
+    # self.write_shellrc()
 
     def write_ssh_file(self):
         template = f'''
@@ -25,17 +28,41 @@ set HOST {self.host}
 set USER {self.user}
 set PASSWORD {self.password}
 
-spawn ssh -p $PORT $USER@$HOST
-''' + '''
-expect {
-        "yes/no" {send "yes\\r";exp_continue;}
-         "*password:*" { send "$PASSWORD\\r" }
-        }
+# Check if the USE_PROXY environment variable is set. If not, default to "yes" (use proxy).
+if {{![info exists ::env(USE_PROXY)]}} {{
+    set ::env(USE_PROXY) "{"yes" if self.use_proxy else "no"}"
+}}
+set use_proxy $::env(USE_PROXY)
+# Proxy settings
+set PROXY_COMMAND "nc -X 5 -x {self.proxy} %h %p"
+
+if {{$use_proxy == "yes"}} {{
+    spawn ssh -p $PORT -o "ProxyCommand $PROXY_COMMAND" $USER@$HOST
+}} else {{
+    spawn ssh -p $PORT $USER@$HOST
+}}
+
+expect {{
+    -re "Are you sure you want to continue connecting.*" {{
+        send "yes\\r"
+        exp_continue
+    }}
+    "*password:*" {{
+        send "$PASSWORD\\r"
+    }}
+}}
+
 interact
         '''
         self.ssh_file_path.parent.mkdir(parents=True, exist_ok=True)
 
         # 创建文件
+        if self.ssh_file_path.is_file():
+            print(f"*******************************************************\n{self.ssh_file_path}文件已存在，正在备份...")
+            back_name = f"{self.ssh_file_path.name}.{int(time.time())}.bak"
+            self.ssh_file_path.rename(self.ssh_file_path.parent / back_name)
+            print(f"=> {self.ssh_file_path.parent / back_name }")
+
         self.ssh_file_path.touch()
         self.ssh_file_path.write_text(template)
         print(f"*******************************************************\n生成{self.ssh_file_path}文件成功!!!")
@@ -94,14 +121,6 @@ interact
 
 
 if __name__ == '__main__':
-    # ssh = SSHBuilder(
-    #     host="121.37.176.33",
-    #     user="root",
-    #     password="Niren1015",
-    #     alias_name="snhw",
-    # )
-    # ssh.build()
-
     while True:
         print("\n请输入你想添加的ssh(按q 或 ctrl+c 退出)")
         alias_name = input("别名：")
@@ -115,6 +134,13 @@ if __name__ == '__main__':
         port = int(port_str) if port_str.isdigit() else 22  # 默认端口为22
         ssh_file_home = input("ssh_file_home(不填即回车 空格默认为~/.ssh/home): ")
         ssh_file_home = ssh_file_home or ""
+        ssh_use_proxy = input(
+            "ssh_use_proxy(不填/回车/空格 默认为false, 使用填true. 或后期配置环境变量 USE_PROXY= true): ")
+        ssh_proxy = ""
+        if ssh_use_proxy.lower() == "true":
+            ssh_use_proxy = True
+            ssh_proxy = input("ssh_proxy(不填即回车 空格默认为127.0.0..gitignore:7890): ")
+        ssh_proxy = ssh_proxy or "127.0.0..gitignore:7890"
 
         shellrc_choice = input("写入的shellrc(可选bash,zsh, 不填即回车bash与zsh全部写入): ").lower()
 
@@ -125,6 +151,8 @@ if __name__ == '__main__':
             alias_name=alias_name,
             port=port,
             file_home=ssh_file_home,
+            use_proxy=ssh_use_proxy,
+            proxy=ssh_proxy
         )
         ssh.write_ssh_file()
         ssh.write_shellrc(shellrc_choice)
